ca8b00c7fb5eaa1b3fab141033a8b17456d775ac
Bennor McCarthy <bennor.mccarthy@akqa.com>
2019-05-30 09:10:32 +1000

    Add working example (before refactoring)

---
+++ GreetingRequest.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace AsyncRefactoringBug
+{
+    public class GreetingRequest : IRequest<string>
+    {
+        public GreetingRequest(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+
+    public class GreetingRequestHandler : IRequestHandler<GreetingRequest, string>
+    {
+        public Task<string> Handle(GreetingRequest request, CancellationToken cancellationToken)
+        {
+            return GetGreeting(request.Name);
+        }
+
+        public static Task<string> GetGreeting(string name)
+        {
+            return Task.FromResult($"Hello, {name ?? "world"}!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AsyncRefactoringBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            var serviceProvider = services.BuildServiceProvider();
+            var mediator = serviceProvider.GetService<IMediator>();
+            Run(mediator, args.FirstOrDefault()).Wait();
+        }
+
+        private static async Task Run(IMediator mediator, string name)
+        {
+            var greeting = await mediator.Send(new GreetingRequest(name));
+            Console.WriteLine(greeting);
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
